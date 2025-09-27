d34f45ed528dc03116e6b3a4f74525d52d41cfaa
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2025-09-27 03:04:36 -0300

    fix: update error message formatting in print_error function

--- src/main.c
+++ src/main.c
@@ -6,15 +6,17 @@
 /*   By: rdos-san <rdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 00:11:55 by rdos-san          #+#    #+#             */
-/*   Updated: 2025/09/27 00:11:55 by rdos-san         ###   ########.fr       */
+/*   Updated: 2025/09/27 02:38:50 by rdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../includes/cub3d.h"
 
 int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        print_error("Usage: ./cub3d <map_file.cub>");
+        print_error("Usage: ./cub3d <map_file.cub>\n");
         return (1);
     }
     validate_file(argv[1]);
--- src/utils/print.c
+++ src/utils/print.c
@@ -6,7 +6,7 @@
 /*   By: rdos-san <rdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 01:05:06 by rdos-san          #+#    #+#             */
-/*   Updated: 2025/09/27 01:05:06 by rdos-san         ###   ########.fr       */
+/*   Updated: 2025/09/27 02:38:30 by rdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 int	print_error(const char *message)
 {
-    const char *completed_message = message + "\n";
-
-    ft_putstr_fd(completed_message, STDERR_FILENO);
+    ft_putstr_fd(message, STDERR_FILENO);
     return (1);
 }
