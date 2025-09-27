a0447cf0fc96b5f891d4ef6a6f950f7a82de0c6e
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2025-09-27 03:09:01 -0300

    feat: add print_error and validate_file function prototypes

--- includes/prototypes.h
+++ includes/prototypes.h
@@ -6,11 +6,17 @@
 /*   By: rdos-san <rdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 00:16:14 by rdos-san          #+#    #+#             */
-/*   Updated: 2025/09/27 00:16:14 by rdos-san         ###   ########.fr       */
+/*   Updated: 2025/09/27 02:34:58 by rdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PROTOTYPES_H
 # define PROTOTYPES_H
+
+// Utils
+int	print_error(const char *message);
+
+// Parser Validations
+int	validate_file(char *filename);
 
 #endif
