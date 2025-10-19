b00bd4377b1f341b8e72557a0ff9cc02c5805d88
anacpp <anacarlacpp@gmail.com>
2025-10-19 18:23:48 -0300

    feat(constants): add window size, keycodes, and movement params for raycasting engine

--- includes/constants.h
+++ includes/constants.h
@@ -13,6 +13,28 @@
 #ifndef CONSTANTS_H
 # define CONSTANTS_H
 
-# define KEY_ESC 65307
+/* Window dimensions */
+# define WINDOW_WIDTH 800
+# define WINDOW_HEIGHT 600
+
+/* Key codes (X11) */
+# define KEY_ESC   65307
+# define KEY_W     119
+# define KEY_A     97
+# define KEY_S     115
+# define KEY_D     100
+# define KEY_LEFT  65361
+# define KEY_RIGHT 65363
+
+/* Map tiles */
+# define TILE_WALL '1'
+# define TILE_EMPTY '0'
+
+/* Field of view helper (plane length ~ tan(FOV/2)) */
+# define CAMERA_PLANE 0.66
+
+/* Movement settings */
+# define MOVE_SPEED 0.05
+# define ROT_SPEED  0.03
 
 #endif
