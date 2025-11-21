885a4c010f67598a23200e060be273149f79da10
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2025-11-21 14:21:40 -0300

    refactor: checks if there are only digits in validate rgb part

--- src/parser/parser_color.c
+++ src/parser/parser_color.c
@@ -6,7 +6,7 @@
 /*   By: rdos-san <rdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 16:58:00 by rdos-san          #+#    #+#             */
-/*   Updated: 2025/10/16 16:59:47 by rdos-san         ###   ########.fr       */
+/*   Updated: 2025/11/21 14:16:07 by rdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,21 @@
 static int	validate_rgb_part(char *part, int *value)
 {
 	char	*trimmed;
+	int		i;
 
 	trimmed = ft_strtrim(part, " \t\n");
 	if (!trimmed || *trimmed == '\0')
 		return (free(trimmed), 0);
+	i = 0;
+	while (trimmed[i])
+	{
+		if (!ft_isdigit(trimmed[i]))
+		{
+			free(trimmed);
+			return (0);
+		}
+		i++;
+	}
 	*value = ft_atoi(trimmed);
 	free(trimmed);
 	if (*value < 0 || *value > 255)
@@ -46,9 +57,9 @@ static int	build_color_from_rgb(char **rgb_values)
 
 int	parse_color_rgb_to_int(const char *str)
 {
-	char	**rgb_values;
-	int		color;
-	int		count;
+	char **rgb_values;
+	int color;
+	int count;
 
 	rgb_values = ft_split(str, ',');
 	if (!rgb_values)
